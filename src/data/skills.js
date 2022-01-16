aa9366d75ec4ab5c8e44073039f4a491138a9010
eo640217 <89750793+eo640217@users.noreply.github.com>
2022-01-16 00:31:51 -0500

    added skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,47 +3,90 @@ export const skills = [
     name: "JavaScript",
     image:
       "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-javascript-is-a-high-level-interpreted-programming-language-logo-color-tal-revivo.png",
+    technologies: [
+      "NodeJS",
+      " ReactJS",
+      " AOS",
+      " Bootstrap",
+      " NextJS",
+      " Gatsby ",
+    ],
   },
   {
     name: "Java",
     image:
       "https://img.icons8.com/color/48/000000/java-coffee-cup-logo--v1.png",
+    technologies: [
+      "Spring",
+      "Web MVC",
+      "AOP",
+      "Hibernate",
+      "Thymeleaf",
+      "JPA",
+      "Spring Security",
+    ],
   },
   {
     name: "HTML",
     image:
       "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page-logo-shadow-tal-revivo.png",
+    technologies: [],
   },
-  { name: "CSS", image: "https://img.icons8.com/color/48/000000/css3.png" },
+  {
+    name: "CSS",
+    image: "https://img.icons8.com/color/48/000000/css3.png",
+    technologies: [],
+  },
   {
     name: "C",
     image: "https://img.icons8.com/color/48/000000/c-programming.png",
+    technologies: [],
   },
   {
     name: "Python",
     image: "https://img.icons8.com/color/48/000000/python--v1.png",
+    technologies: ["Pandas", "NumPy", "TensorFlow", "scikit-learn", "selenium"],
   },
   {
     name: "C++",
     image: "https://img.icons8.com/color/48/000000/c-plus-plus-logo.png",
+    technologies: ["STL"],
   },
-  { name: "Git", image: "https://img.icons8.com/color/48/000000/git.png" },
+  {
+    name: "Git",
+    image: "https://img.icons8.com/color/48/000000/git.png",
+    technologies: [],
+  },
   {
     name: "No SQL",
     image: "https://img.icons8.com/color/48/000000/mongodb.png",
+    technologies: ["MongoDB"],
   },
   {
     name: "SQL",
     image: "https://img.icons8.com/color/48/000000/postgreesql.png",
+    technologies: ["PostgresSQL", "MySql"],
   },
 
   {
     name: "Teamwork",
     image: "https://img.icons8.com/color/48/000000/teamwork--v2.png",
+    technologies: [],
   },
   {
-    name: "Azure",
+    name: "Cloud Computing",
     image: "https://img.icons8.com/color/48/000000/azure-1.png",
+    technologies: ["Azure"],
+  },
+  {
+    name: "C#",
+    image: "https://img.icons8.com/color/48/000000/c-sharp-logo.png",
+    technologies: ["ASP.NET Core WebAPI", "DotNet CLI"],
+  },
+  {
+    name: "TypeScript",
+    image: "https://img.icons8.com/color/50/000000/typescript.png",
+    technologies: [],
   },
 ];
 export default skills;
